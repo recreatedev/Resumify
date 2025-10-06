97cb560dd8ffbd24d4dd3150dab8c7a62dea1b03
arun <ptech784@gmail.com>
2025-10-06 22:44:05 +0530

    feat:change env prefix

--- apps/backend/internal/config/config.go
+++ apps/backend/internal/config/config.go
@@ -62,8 +62,8 @@ func LoadConfig() (*Config, error) {
 
 	k := koanf.New(".")
 
-	err := k.Load(env.Provider("BOILERPLATE_", ".", func(s string) string {
-		return strings.ToLower(strings.TrimPrefix(s, "BOILERPLATE_"))
+	err := k.Load(env.Provider("RESUMIFY_", ".", func(s string) string {
+		return strings.ToLower(strings.TrimPrefix(s, "RESUMIFY_"))
 	}), nil)
 	if err != nil {
 		logger.Fatal().Err(err).Msg("could not load initial env variables")
